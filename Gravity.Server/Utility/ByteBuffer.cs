ca3e4eeb6c66e367247a179405e5aebacb5af02e
Martin Halliday <martin.bikeman@gmail.com>
2020-03-08 19:09:11 -0800

    ByteBuffer testing and debugging

--- Gravity.Server/Utility/ByteBuffer.cs
+++ Gravity.Server/Utility/ByteBuffer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gravity.Server.Interfaces;
+using System;
 
 namespace Gravity.Server.Utility
 {
@@ -100,6 +101,25 @@ namespace Gravity.Server.Utility
         }
 
         /// <summary>
+        /// Tests whether the Replace method can be used to replace bytes in this
+        /// buffer. Note that this method returnd True for the case where the
+        /// replacement continues into subsequent buffers, i.e. the whole replacement
+        /// operation is not confined to just this buffer.
+        /// </summary>
+        /// <param name="offset">The offset into this buffer to start replacing</param>
+        /// <param name="count">The number of bytes to replace</param>
+        /// <param name="replacementCount">The number of bytes to replace them with</param>
+        /// <returns></returns>
+        public bool CanReplace(int offset, int count, int replacementCount)
+        {
+            var additionalBytes = replacementCount - count;
+            if (additionalBytes <= 0) return true;
+
+            if (Start + offset + count >= End) return true;
+            return additionalBytes <= TailSize;
+        }
+
+        /// <summary>
         /// Replaces a range of bytes from the data array with some new byte and
         /// updates the input parameters to allow the replacement to continue in the
         /// next buffer
@@ -107,7 +127,7 @@ namespace Gravity.Server.Utility
         /// <param name="replacementBytes">The array of bytes to write into this buffer. Can be
         /// null or zero length to simply delete bytes from the buffer</param>
         /// <param name="offset">The offset into this buffer to start overwritng. Returns
-        /// zero because this is where you would start overwriting in the next buffer</param>
+        /// the offset where you would write the next chunk or 0 if this buffer is full</param>
         /// <param name="count">The number of bytes to overwrite. Can be 0 to insert bytes
         /// without overwriting existing ones. Returns the number of bytes remaining to
         /// be overwritten in the next buffer</param>
@@ -125,48 +145,239 @@ namespace Gravity.Server.Utility
 #if DEBUG
             if (HeadSize + offset < 0) throw new ArgumentException("Offset is before buffer start");
 #endif
-            if (count == replacementCount && offset >= 0)
+            if (count == replacementCount)
             {
-                // Straight overwrite with no moving bytes around
-
-                var bytesToCopy = Start + offset + count > End 
-                    ? End - Start - offset
-                    : count;
-
-                Array.Copy(replacementBytes, replacementStart, Data, Start + offset, bytesToCopy);
-
-                offset = 0;
-                count -= bytesToCopy;
-                replacementStart += bytesToCopy;
-                replacementCount -= bytesToCopy;
-
+                ReplaceOwerwrite(replacementBytes, ref offset, ref count, ref replacementStart, ref replacementCount);
                 return;
             }
 
             var tailBytes = End - Start - offset - count;
+            var headBytes = offset;
 
             if (count < replacementCount)
             {
-                var extraBytes = replacementCount - count;
+                ReplaceInsert(replacementBytes, ref offset, ref count, ref replacementStart, ref replacementCount, tailBytes, headBytes);
+            }
+            else
+            {
+                ReplaceDelete(replacementBytes, ref offset, ref count, ref replacementStart, ref replacementCount, tailBytes, headBytes);
+            }
+        }
+
+        /// <summary>
+        /// Splits this buffer into two buffers and inserts a large number of bytes
+        /// in the middle of the data
+        /// </summary>
+        /// <param name="offset">The offset of the first byte to copy into the new buffer</param>
+        /// <param name="bytesToDelete">The number of bytes to delete starting at offset</param>
+        /// <param name="bufferPool">This will be used to make the new byte buffer</param>
+        /// <param name="newBytes">The bytes to insert</param>
+        /// <param name="newBytesStart">The offset into newBytes to start copying</param>
+        /// <param name="newBytesCount">The number of bytes to insert from newBytes</param>
+        /// <returns>A new ByteBuffer containing the second half of the data</returns>
+        public ByteBuffer Insert(
+            int offset, 
+            int bytesToDelete,
+            IBufferPool bufferPool,
+            byte[] newBytes,
+            int newBytesStart,
+            int newBytesCount)
+        {
+            var moveStartIndex = HeadSize + offset + bytesToDelete;
+#if DEBUG
+            if (moveStartIndex < 0) throw new ArgumentException("Offset is before buffer start");
+#endif
+            var bytesToMove = End - moveStartIndex;
+#if DEBUG
+            if (bytesToMove < 0) throw new ArgumentException("Deleting more bytes than the buffer contains");
+#endif
+            if (newBytesCount <= TailSize + bytesToDelete)
+            {
+                if (bytesToMove > 0)
+                    Array.Copy(Data, moveStartIndex, Data, HeadSize + offset + newBytesCount, bytesToMove);
+
+                Array.Copy(newBytes, newBytesStart, Data, HeadSize + offset, newBytesCount);
+                End += newBytesCount - bytesToDelete;
+                return null;
+            }
+            
+            var result = new ByteBuffer(bufferPool.GetAtLeast(newBytesCount + bytesToMove), 0);
+            result.Append(newBytes, newBytesStart, newBytesCount);
+
+            if (bytesToMove > 0)
+                result.Append(Data, moveStartIndex, bytesToMove);
+
+            End -= bytesToMove + bytesToDelete;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Deletes some bytes from the buffer
+        /// </summary>
+        /// <param name="offset">Offset into the buffer to start deleting. Returns 0, which
+        /// is where the deleting should contine in the next buffer</param>
+        /// <param name="count">The number of bytes to delete. Returns with the number of 
+        /// bytes to delete from the next buffer</param>
+        public void Delete(
+            ref int offset,
+            ref int count)
+        {
+            var replacementStart = 0;
+            var replacementCount = 0;
+            Replace(null, ref offset, ref count, ref replacementStart, ref replacementCount);
+        }
+
+        /// <summary>
+        /// Handles the case where the replacement bytes are longer and
+        /// therefore need to be inserted into the buffer
+        /// </summary>
+        private void ReplaceInsert(
+            byte[] replacementBytes, 
+            ref int offset, 
+            ref int count, 
+            ref int replacementStart, 
+            ref int replacementCount, 
+            int tailBytes, 
+            int headBytes)
+        {
+            void moveHead(
+                ref int o,
+                ref int c,
+                ref int rs,
+                ref int rc)
+            {
+            };
+
+            void moveTail(
+                ref int o,
+                ref int c,
+                ref int rs,
+                ref int rc)
+            {
+                var extraBytes = rc - c;
                 if (tailBytes > 0)
                 {
                     var tailOffset = End - tailBytes;
                     Array.Copy(Data, tailOffset, Data, tailOffset + extraBytes, tailBytes);
                 }
-                Array.Copy(replacementBytes, replacementStart, Data, Start + offset, replacementCount);
+                Array.Copy(replacementBytes, rs, Data, Start + o, rc);
                 End += extraBytes;
+                o += rc;
+            };
+
+            if (headBytes < tailBytes)
+            {
+                moveHead(ref offset, ref count, ref replacementStart, ref replacementCount);
+                moveTail(ref offset, ref count, ref replacementStart, ref replacementCount);
             }
             else
             {
-                var deletedBytes = count - replacementCount;
-                Array.Copy(replacementBytes, replacementStart, Data, Start + offset, replacementCount);
-                if (tailBytes > 0)
-                {
-                    var tailOffset = End - tailBytes;
-                    Array.Copy(Data, tailOffset, Data, tailOffset - deletedBytes, tailBytes);
-                }
-                End -= deletedBytes;
+                moveTail(ref offset, ref count, ref replacementStart, ref replacementCount);
+                moveHead(ref offset, ref count, ref replacementStart, ref replacementCount);
             }
         }
+
+        private void ReplaceDelete(
+            byte[] replacementBytes, 
+            ref int offset, 
+            ref int count, 
+            ref int replacementStart, 
+            ref int replacementCount, 
+            int tailBytes, 
+            int headBytes)
+        {
+            var bytesToDelete = count;
+
+            if (Start + offset + count > End)
+                bytesToDelete = End - Start - offset;
+
+            if (tailBytes > 0 && headBytes < tailBytes)
+            {
+                var availableSpace = Start + offset + bytesToDelete;
+
+                if (headBytes > 0)
+                    availableSpace -= headBytes;
+
+                var bytesToReplace = replacementCount;
+
+                if (bytesToReplace > availableSpace)
+                    bytesToReplace = availableSpace;
+
+                var start = Start + offset + bytesToDelete - bytesToReplace;
+
+                if (headBytes > 0)
+                {
+                    start -= headBytes;
+                    Array.Copy(Data, Start, Data, start, headBytes);
+                }
+
+                if (bytesToReplace > 0)
+                {
+                    Array.Copy(replacementBytes, replacementStart, Data, Start + offset, bytesToReplace);
+                    replacementStart += bytesToReplace;
+                    replacementCount -= bytesToReplace;
+                }
+
+                Start = start;
+            }
+            else
+            {
+                var availableSpace = Size - offset;
+
+                if (tailBytes > 0)
+                    availableSpace -= tailBytes;
+
+                var bytesToReplace = replacementCount;
+
+                if (bytesToReplace > availableSpace)
+                    bytesToReplace = availableSpace;
+
+                var end  = Start + offset + bytesToReplace;
+
+                if (tailBytes > 0)
+                {
+                    Array.Copy(Data, End - tailBytes, Data, end, tailBytes);
+                    end += tailBytes;
+                }
+
+                if (bytesToReplace > 0)
+                {
+                    Array.Copy(replacementBytes, replacementStart, Data, Start + offset, bytesToReplace);
+                    replacementStart += bytesToReplace;
+                    replacementCount -= bytesToReplace;
+                }
+
+                End = end;
+            }
+
+            count -= bytesToDelete;
+            offset = 0;
+        }
+
+        private void ReplaceOwerwrite(
+            byte[] replacementBytes, 
+            ref int offset, 
+            ref int count, 
+            ref int replacementStart, 
+            ref int replacementCount)
+        {
+            var bytesToCopy = Start + offset + count > End
+                ? End - Start - offset
+                : count;
+
+            Array.Copy(replacementBytes, replacementStart, Data, Start + offset, bytesToCopy);
+
+            offset += bytesToCopy;
+            count -= bytesToCopy;
+            replacementStart += bytesToCopy;
+            replacementCount -= bytesToCopy;
+
+            if (offset + Start >= End)
+                offset = 0;
+
+            return;
+        }
+
     }
 }
--- Gravity.UnitTests/Utility/ByteBufferTests.cs
+++ Gravity.UnitTests/Utility/ByteBufferTests.cs
@@ -1,4 +1,5 @@
-﻿using Gravity.Server.Utility;
+﻿using Gravity.Server.Interfaces;
+using Gravity.Server.Utility;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -117,6 +118,401 @@ namespace Gravity.UnitTests.Utility
             Assert.AreEqual(4, dest[3]);
             Assert.AreEqual(5, dest[4]);
             Assert.AreEqual(6, dest[5]);
+        }
+
+        [Test]
+        public void Should_replace_at_start()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 0;
+            var count = 3;
+            var replacementStart = 0;
+            var replacementCount = 3;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(new byte[] { 8, 9, 10}, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(3, offset);
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(3, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+
+            var dest = new byte[6];
+            bb.Get(0, 6, dest, 0);
+
+            Assert.AreEqual(8, dest[0]);
+            Assert.AreEqual(9, dest[1]);
+            Assert.AreEqual(10, dest[2]);
+            Assert.AreEqual(4, dest[3]);
+            Assert.AreEqual(5, dest[4]);
+            Assert.AreEqual(6, dest[5]);
+        }
+
+        [Test]
+        public void Should_replace_at_end()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 3;
+            var count = 3;
+            var replacementStart = 0;
+            var replacementCount = 3;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(new byte[] { 8, 9, 10}, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(3, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+
+            var dest = new byte[6];
+            bb.Get(0, 6, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(3, dest[2]);
+            Assert.AreEqual(8, dest[3]);
+            Assert.AreEqual(9, dest[4]);
+            Assert.AreEqual(10, dest[5]);
+        }
+
+        [Test]
+        public void Should_replace_overlapping_end()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 4;
+            var count = 3;
+            var replacementStart = 0;
+            var replacementCount = 3;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(new byte[] { 8, 9, 10}, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(1, count);
+            Assert.AreEqual(2, replacementStart);
+            Assert.AreEqual(1, replacementCount);
+
+            var dest = new byte[6];
+            bb.Get(0, 6, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(3, dest[2]);
+            Assert.AreEqual(4, dest[3]);
+            Assert.AreEqual(8, dest[4]);
+            Assert.AreEqual(9, dest[5]);
+        }
+
+        [Test]
+        public void Should_replace_larger_buffer()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 2;
+            var count = 10;
+            var replacementStart = 0;
+            var replacementCount = 10;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(new byte[] { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }, 
+                ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(6, count);
+            Assert.AreEqual(4, replacementStart);
+            Assert.AreEqual(6, replacementCount);
+
+            var dest = new byte[6];
+            bb.Get(0, 6, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(8, dest[2]);
+            Assert.AreEqual(9, dest[3]);
+            Assert.AreEqual(10, dest[4]);
+            Assert.AreEqual(11, dest[5]);
+        }
+
+        [Test]
+        public void Should_insert_within()
+        {
+            var bufferPool = SetupMock<IBufferPool>();
+
+            const int size = 10;
+            var bb1 = new ByteBuffer(new byte[size], 0);
+
+            bb1.Append(new byte[] { 1, 2, 3, 4 }, 0, 4);
+            var bb2 = bb1.Insert(1, 2, bufferPool, new byte[] { 8, 9, 10 }, 0, 3);
+
+            Assert.IsNull(bb2);
+            Assert.AreEqual(5, bb1.Length);
+
+            var dest = new byte[5];
+            bb1.Get(0, 5, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(8, dest[1]);
+            Assert.AreEqual(9, dest[2]);
+            Assert.AreEqual(10, dest[3]);
+            Assert.AreEqual(4, dest[4]);
+        }
+
+        [Test]
+        public void Should_insert_at_end()
+        {
+            var bufferPool = SetupMock<IBufferPool>();
+
+            const int size = 10;
+            var bb1 = new ByteBuffer(new byte[size], 0);
+
+            bb1.Append(new byte[] { 1, 2, 3, 4 }, 0, 4);
+            var bb2 = bb1.Insert(2, 2, bufferPool, new byte[] { 8, 9, 10, 11, 12 }, 0, 5);
+
+            Assert.IsNull(bb2);
+            Assert.AreEqual(7, bb1.Length);
+
+            var dest = new byte[7];
+            bb1.Get(0, 7, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(8, dest[2]);
+            Assert.AreEqual(9, dest[3]);
+            Assert.AreEqual(10, dest[4]);
+            Assert.AreEqual(11, dest[5]);
+            Assert.AreEqual(12, dest[6]);
+        }
+
+        [Test]
+        public void Should_insert_large_at_end()
+        {
+            var bufferPool = SetupMock<IBufferPool>();
+
+            const int size = 10;
+            var bb1 = new ByteBuffer(new byte[size], 0);
+
+            bb1.Append(new byte[] { 1, 2, 3, 4 }, 0, 4);
+            var bb2 = bb1.Insert(2, 2, bufferPool, new byte[] { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }, 0, 10);
+
+            var expected = new byte[] { 1, 2, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+            Assert.IsNotNull(bb2);
+            Assert.AreEqual(expected.Length, bb1.Length + bb2.Length);
+
+            var actual = new byte[bb1.Length + bb2.Length];
+            bb1.Get(0, bb1.Length, actual, 0);
+            bb2.Get(0, bb2.Length, actual, bb1.Length);
+
+            for (var i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+        }
+
+        [Test]
+        public void Should_insert_large_in_middle()
+        {
+            var bufferPool = SetupMock<IBufferPool>();
+
+            const int size = 10;
+            var bb1 = new ByteBuffer(new byte[size], 0);
+
+            bb1.Append(new byte[] { 1, 2, 3, 4 }, 0, 4);
+            var bb2 = bb1.Insert(1, 2, bufferPool, new byte[] { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 }, 0, 10);
+
+            var expected = new byte[] { 1, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 4 };
+
+            Assert.IsNotNull(bb2);
+            Assert.AreEqual(expected.Length, bb1.Length + bb2.Length);
+
+            var actual = new byte[bb1.Length + bb2.Length];
+            bb1.Get(0, bb1.Length, actual, 0);
+            bb2.Get(0, bb2.Length, actual, bb1.Length);
+
+            for (var i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+        }
+
+        [Test]
+        public void Should_delete_within()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 2;
+            var count = 2;
+            var replacementStart = 0;
+            var replacementCount = 0;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(null, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(0, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+            Assert.AreEqual(4, bb.Length);
+
+            var dest = new byte[4];
+            bb.Get(0, 4, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(5, dest[2]);
+            Assert.AreEqual(6, dest[3]);
+        }
+
+        [Test]
+        public void Should_delete_at_end()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 3;
+            var count = 3;
+            var replacementStart = 0;
+            var replacementCount = 0;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(null, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(0, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+            Assert.AreEqual(3, bb.Length);
+
+            var dest = new byte[bb.Length];
+            bb.Get(0, bb.Length, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(3, dest[2]);
+        }
+
+        [Test]
+        public void Should_delete_at_start()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 0;
+            var count = 3;
+            var replacementStart = 0;
+            var replacementCount = 0;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(null, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(0, count);
+            Assert.AreEqual(0, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+            Assert.AreEqual(3, bb.Length);
+            Assert.AreEqual(3, bb.HeadSize);
+
+            var dest = new byte[3];
+            bb.Get(0, 3, dest, 0);
+
+            Assert.AreEqual(4, dest[0]);
+            Assert.AreEqual(5, dest[1]);
+            Assert.AreEqual(6, dest[2]);
+        }
+
+        [Test]
+        public void Should_delete_spanning_buffers()
+        {
+            const int size = 10;
+
+            var bb1 = new ByteBuffer(new byte[size], 0);
+            var bb2 = new ByteBuffer(new byte[size], 0);
+
+            bb1.Append(new byte[] { 1, 2, 3, 4, 5 }, 0, 5);
+            bb2.Append(new byte[] { 6, 7, 8, 9, 10 }, 0, 5);
+
+            var replacementBytes = new byte[] { 90, 91, 92 };
+
+            var offset = 4;
+            var count = 5;
+            var replacementStart = 0;
+            var replacementCount = replacementBytes.Length;
+
+            Assert.IsTrue(bb1.CanReplace(offset, count, replacementCount));
+
+            bb1.Replace(replacementBytes, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.IsTrue(bb2.CanReplace(offset, count, replacementCount));
+
+            bb2.Replace(replacementBytes, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            var expected = new byte[] { 1, 2, 3, 4, 90, 91, 92, 10 };
+
+            Assert.AreEqual(expected.Length, bb1.Length + bb2.Length);
+
+            var actual = new byte[bb1.Length + bb2.Length];
+            bb1.Get(0, bb1.Length, actual, 0);
+            bb2.Get(0, bb2.Length, actual, bb1.Length);
+
+            for (var i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i]);
+        }
+
+        [Test]
+        public void Should_delete_large()
+        {
+            const int size = 10;
+            var bb = new ByteBuffer(new byte[size], 0);
+
+            bb.Append(new byte[] { 1, 2, 3, 4, 5, 6 }, 0, 6);
+
+            var offset = 3;
+            var count = 8;
+            var replacementStart = 0;
+            var replacementCount = 0;
+
+            Assert.IsTrue(bb.CanReplace(offset, count, replacementCount));
+
+            bb.Replace(null, ref offset, ref count, ref replacementStart, ref replacementCount);
+
+            Assert.AreEqual(0, offset);
+            Assert.AreEqual(5, count);
+            Assert.AreEqual(0, replacementStart);
+            Assert.AreEqual(0, replacementCount);
+            Assert.AreEqual(3, bb.Length);
+
+            var dest = new byte[3];
+            bb.Get(0, 3, dest, 0);
+
+            Assert.AreEqual(1, dest[0]);
+            Assert.AreEqual(2, dest[1]);
+            Assert.AreEqual(3, dest[2]);
         }
     }
 }
